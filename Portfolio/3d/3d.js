b8e43075efe14512ebd86a26bf4c1433ebbf9994
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-07-08 07:46:47 +0100

    Commit

---
+++ Portfolio/3d/3d.js
@@ -0,0 +1,12 @@
+/*At tablet size query all selector and set their grid column to be same as their grid row*/
+
+// var screenWidth = window.innerWidth;
+
+// if (screenWidth <= 600){
+//     var divElements = document.querySelectorAll('.item');
+
+// divElements.forEach(function(element) {
+//   element.classList.remove('item--1x1', 'item--1x2', 'item--2x1', 'item--2x2');
+// });
+// }
+
