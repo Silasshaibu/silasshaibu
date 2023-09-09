9e5720cc6632c8e0ae10585ba22af307b1248817
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-09-09 23:29:14 +0100

    commit

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-//onClick let user be directed to that project link
 
-// const project = document.getElementById('');
-function projectlink(){
-    let projectLinkURL = document.getSelection;
-    console.log(projectLinkURL);
+    const anchorTags = document.querySelectorAll('.portfolio-gigImage a');
+
+    // Attach click event listener to each anchor tag
+    anchorTags.forEach((anchorTag) => {
+    anchorTag.addEventListener('click', (event) => {
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Get the href attribute value of the clicked anchor tag
+        const link = anchorTag.href;
+
+        // Redirect to the link
+        window.location.href = link;
+    });
+    });
 }
+
+
+
+
