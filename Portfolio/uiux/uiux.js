eec6291b6056b9287ef860aca590ac815fe517b8
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-09-10 23:27:34 +0100

    commit

---
+++ Portfolio/uiux/uiux.js
@@ -0,0 +1,15 @@
+
+// console.log(45);
+
+
+//if the users is on tablet
+//only tablet and mobile options should be allowed to be toggled. pc option should be greyed out or disabled for click, there should be a tip that says to view the ui ux of a higher screen please view on a higher screen. or click to download the ui ux in pdf format. //provide the user a pdf that contains the uiux but not all of the pages jus the preview pages
+
+
+//if the users is on mobile
+//only mobile options should be allowed to be toggled,
+//others should be greyed out and also provide a tip that notifies the user that uiux of other devices can be viewed with devices on higher width. and also user cand download the pdf format of the uiux instead.
+
+//if the users is on mobile
+//only mobile options should be allowed to be toggled,
+//others should be greyed out and also provide a tip that notifies the user that uiux of other devices can be viewed with devices on higher width. and also user cand download the pdf format of the uiux instead.
