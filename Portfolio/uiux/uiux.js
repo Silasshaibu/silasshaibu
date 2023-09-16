e27ab4b5c364d3e3ca5be7fc0ec8d10c9fcc9dae
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-09-16 22:35:53 +0100

    commit

--- Portfolio/uiux/uiux.js
+++ Portfolio/uiux/uiux.js
@@ -8,7 +8,6 @@ const indImgWidthAll = document.querySelectorAll(".slide");
 let viewerWidth = viewerFrame.offsetWidth;
 let viewerHeight = viewerFrame.offsetHeight;
 
-
 // RESIZE WINDOW - Check if the window width is less than certain width, the upper devices toggle button should be disabled
 window.addEventListener('resize', () => {
   pcButton.style.display = (window.innerWidth < 1024) ? "none" : "inline-block";
@@ -23,11 +22,14 @@ window.addEventListener('load', () => {
 
   slideContainer.style.width = `${viewerWidth * slideContainer.childElementCount }px`;
 
-
   Slides.forEach(slide => {
     slide.classList.contains('desktop--version') || slide.classList.contains('mobile--version') ? slide.style.display = 'none' : slide.style.display = 'inline-block';
+
+    // //identifying the active slide or current slide
+    // slide.classList.contains('active')?console.log(slide): console.log('do nothing') ;
   });
   console.log("Width of ScreenViewer: " + viewerWidth + " pixels");
+
 });
 
 let defTanslateInterfval = 0;
@@ -37,17 +39,40 @@ Slides = document.querySelectorAll('.slide');
 //RIGHT ARROW
 function rightArrow(){
   let slideWidth = viewerFrame.offsetWidth;
+  let numOfSlidesCompatibleWithCurrentDevice = 0;
 
   Slides.forEach(slide => {
     slide.width = slideWidth;
-  });
-  console.log(`Slide width is ${slideWidth}px`);
 
+
+    //if this current slide does not have a previousElementSibling with a class that matches the current div then it is the first slide.
+
+     // console.log(slide.nextElementSibling);
+      // console.log(slide);
+      // console.log(slide.parentElement);
+
+
+    if (slide.classList.contains('active') && (slide.previousElementSibling)){
+        console.log(slide);
+        console.log('this was the first element')
+      }
+      ///CONTINUE FROM HERE 🏆
+
+    //Below is to get the number of availabe slides for the current viewer}
+    if (slide.style.display === 'inline-block'){
+      numOfSlidesCompatibleWithCurrentDevice++;
+    }
+    //get the lastElementsSibling that has a class of  that matches the current device e.g tablet
+    //
+  });
+
+  //if slide do not have a previous element or previous slibling,
+  //then create one for it at its previous position
+
+  //if current slide has active class, get the slide position
   //translate the slideContainer by a decrement of a slide width
   defTanslateInterfval -= slideWidth;
-
   slideContainer.style.transform =`translateX(${defTanslateInterfval}px)`;
-  console.log(defTanslateInterfval);
 }
 
 
@@ -128,6 +153,8 @@ function toggleDeviceOptions() {
   });
 }
 
+
+//localStorage 
 
 
 
