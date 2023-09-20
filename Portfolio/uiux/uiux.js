e974ef783c9ffa70c41d2de5ca5496f75803e4fc
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-09-20 16:00:51 +0100

    commit

--- Portfolio/uiux/uiux.js
+++ Portfolio/uiux/uiux.js
@@ -11,6 +11,8 @@ const aspectRatios = {
 const projects = {
   project1: {
     name: "Bimto",
+    tools: "Adobe XD",
+    description:"Ecommerce",
     liveWebsite: "http://www.google.com",
     fileDownloadLink: "bimto-uiux.pdf",
     tablet: [
@@ -28,6 +30,8 @@ const projects = {
   },
   project2: {
     name: "AskMeAbroad",
+    tools: "Adobe XD",
+    description:"Educational",
     liveWebsite: "http://www.askmeabroad.com",
     fileDownloadLink: "",
     tablet: [
@@ -58,8 +62,57 @@ function populateProjectList() {
       const listItem = document.createElement('li');
       listItem.dataset.project = projectKey;
 
-      // Set the text content of the list item to the project name
-      listItem.textContent = project.name;
+      // Create a div for the project details
+      const projectDetails = document.createElement('div');
+      projectDetails.classList.add('project-details');
+
+      // Create spans for the project name, description, and tools
+      const projectName = document.createElement('span');
+      projectName.textContent = `${project.name}`;
+
+      const projectDescription = document.createElement('p');
+      projectDescription.textContent = `Type: ${project.description}`;
+
+      const projectTools = document.createElement('span');
+      projectTools.textContent = `Tools: ${project.tools}`;
+
+      // Append the spans to the project details div
+      projectDetails.appendChild(projectName);
+      projectDetails.appendChild(projectDescription);
+      projectDetails.appendChild(projectTools);
+
+      // Create a div for the action icons
+      const actionIcons = document.createElement('div');
+      actionIcons.classList.add('action-icons');
+
+      // Create an SVG icon for the live website link
+      if (project.liveWebsite) {
+        const webIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        webIcon.classList.add('svgListStyle', 'web');
+        webIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        webIcon.setAttribute('width', '24');
+        webIcon.setAttribute('height', '24');
+        webIcon.setAttribute('viewBox', '0 0 24 24');
+        webIcon.setAttribute('stroke-width', '1.5');
+        webIcon.setAttribute('stroke', 'currentColor');
+
+        const webPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        webPath.setAttribute('stroke-linecap', 'round');
+        webPath.setAttribute('stroke-linejoin', 'round');
+        webPath.setAttribute('d', 'M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418');
+
+        webIcon.appendChild(webPath);
+
+        webIcon.addEventListener('click', () => {
+          window.open(project.liveWebsite, '_blank');
+        });
+
+        actionIcons.appendChild(webIcon);
+      }
+
+      // Append the project details and action icons to the list item
+      listItem.appendChild(projectDetails);
+      listItem.appendChild(actionIcons);
 
       // Append the list item to the project list
       projectList.appendChild(listItem);
@@ -122,6 +175,7 @@ function updateImages(imageList) {
 function setActiveDevice(device) {
   activeDevice = device;
 }
+
 // Function to set the default project, device, and display images
 function setDefaultProjectAndDevice() {
   // Select the first project in the list and mark it as active
@@ -149,10 +203,6 @@ function setDefaultProjectAndDevice() {
 // Call the function to set the default project and device after the DOM has loaded
 document.addEventListener('DOMContentLoaded', setDefaultProjectAndDevice);
 
-
-// Call the function to set the default project and device after the DOM has loaded
-document.addEventListener('DOMContentLoaded', setDefaultProjectAndDevice);
-
 // Event listeners for device options (tablet, mobile, pc)
 const deviceOptions = document.querySelectorAll('nav li');
 deviceOptions.forEach((option) => {
@@ -172,10 +222,6 @@ deviceOptions.forEach((option) => {
     updateImages(projects[activeProject][device]);
   });
 });
-
-// JavaScript code for your UI/UX Portfolio
-
-// ... (Previous code remains unchanged) ...
 
 // Function to navigate images
 function navigateImage(direction) {
