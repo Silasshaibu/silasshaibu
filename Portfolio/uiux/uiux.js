72d8cc93270d58aecbe76db5b44f4155c79e6382
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-09-20 13:21:39 +0100

    commit

--- Portfolio/uiux/uiux.js
+++ Portfolio/uiux/uiux.js
@@ -17,40 +17,48 @@ const aspectRatios = {
 // Define project data with images for each device
 const projects = {
   project1: {
-      tablet: [
-          '1024x768_Bimteo_01_Tablet.jpg',
-          '1024x768_Bimteo_02_Tablet.jpg'
-      ],
-      mobile: [
-          '768x384_Bimteo_01_Mobile.jpg',
-          '768x384_Bimteo_01_Mobile.jpg'
-      ],
-      pc: [
-          '1920x1080_Bimteo_01_desktop.jpg',
-          '1920x1080_Bimteo_02_desktop.jpg'
-      ]
+    name:"Bimto",
+    liveWebsite:"www.google.com",
+    fileDownloadLink:"bimto-uiux.pdf",
+    tablet: [
+      '1024x768_Bimteo_01_Tablet.jpg',
+      '1024x768_Bimteo_02_Tablet.jpg'
+    ],
+    mobile: [
+      '768x384_Bimteo_01_Mobile.jpg',
+      '768x384_Bimteo_01_Mobile.jpg'
+    ],
+    pc: [
+      '1920x1080_Bimteo_01_desktop.jpg',
+      '1920x1080_Bimteo_02_desktop.jpg'
+    ]
   },
   project2: {
-      tablet: [
-        '1024x768_Bimto_01_Tablet.jpg',
-        '1024x768_Bimto_02_Tablet.jpg'
-      ],
-      mobile: [
-        '768x384_Bimto_01_Mobile.jpg',
-        '768x384_Bimto_01_Mobile.jpg'
-      ],
-      pc: [
-        '1920x1080_Bimto_01_desktop.jpg',
-        '1920x1080_Bimto_02_desktop.jpg'
-      ]
+    name:"AskMeAbroad",
+    liveWebsite:"www.google.com",
+    fileDownloadLink:"www.pdf.com",
+    tablet: [
+      '1024x768_Bimto_01_Tablet.jpg',
+      '1024x768_Bimto_02_Tablet.jpg'
+    ],
+    mobile: [
+      '768x384_Bimto_01_Mobile.jpg',
+      '768x384_Bimto_01_Mobile.jpg'
+    ],
+    pc: [
+      '1920x1080_Bimto_01_desktop.jpg',
+      '1920x1080_Bimto_02_desktop.jpg'
+    ]
   }
 };
 
 // Function to update the aspect ratio of the image container
+
 function updateAspectRatio(device) {
-  const imageContainer = document.getElementById('image-container');
-  imageContainer.style.aspectRatio = aspectRatios[device];
+  const imageCustomize = document.querySelector('.deviceViewer--imageCustomize');
+  imageCustomize.style.aspectRatio = aspectRatios[device];
 }
+
 
 // Function to navigate images
 function navigateImage(direction) {
@@ -59,9 +67,9 @@ function navigateImage(direction) {
 
   // Ensure the index stays within bounds
   if (currentIndex < 0) {
-      currentIndex = images.length - 1;
+    currentIndex = images.length - 1;
   } else if (currentIndex >= images.length) {
-      currentIndex = 0;
+    currentIndex = 0;
   }
 
   images[currentIndex].classList.add('active'); // Add the 'active' class to the new current image
@@ -80,31 +88,61 @@ rightArrow.addEventListener('click', () => {
 // Event listeners for project list items
 projectListItems.forEach((item) => {
   item.addEventListener('click', () => {
-      const project = item.dataset.project;
-      // Highlight the selected project
-      projectListItems.forEach((li) => {
-          li.classList.remove('active');
-      });
-      item.classList.add('active');
-      // Update the image list based on the selected project and device
-      updateImages(projects[project][activeDevice]);
+    const project = item.dataset.project;
+    // Highlight the selected project
+    projectListItems.forEach((li) => {
+      li.classList.remove('active');
+    });
+    item.classList.add('active');
+    // Update the image list based on the selected project and device
+    updateImages(projects[project][activeDevice]);
   });
+
+  // Add an event listener to the download SVG icon within each project list item
+  const downloadIcon = item.querySelector('.svgListStyle.download');
+  if (downloadIcon) {
+    downloadIcon.addEventListener('click', () => {
+      const project = item.dataset.project;
+      const fileDownloadLink = projects[project].fileDownloadLink;
+      if (fileDownloadLink) {
+        // Create an anchor element to trigger the download
+        const downloadLink = document.createElement('a');
+        downloadLink.href = fileDownloadLink;
+        downloadLink.download = `${projects[project].name}_download.pdf`; // Customize the file name if needed
+        downloadLink.style.display = 'none';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      }
+    });
+  }
+
+  // Add an event listener to the web SVG icon within each project list item
+  const webIcon = item.querySelector('.svgListStyle.web');
+  if (webIcon) {
+    webIcon.addEventListener('click', () => {
+      // Open a new window or tab with the fixed URL
+      window.open('http://www.google.com', '_blank');
+    });
+  }
 });
+
+
 
 // Event listeners for device options (tablet, mobile, pc)
 deviceOptions.forEach((option) => {
   option.addEventListener('click', () => {
-      const device = option.dataset.device;
-      // Highlight the selected device option
-      deviceOptions.forEach((li) => {
-          li.classList.remove('active');
-      });
-      option.classList.add('active');
-      // Update the active device and adjust aspect ratio
-      setActiveDevice(device);
-      // Update the image list based on the selected project and device
-      const activeProject = document.querySelector('#project-list li.active').dataset.project;
-      updateImages(projects[activeProject][device]);
+    const device = option.dataset.device;
+    // Highlight the selected device option
+    deviceOptions.forEach((li) => {
+      li.classList.remove('active');
+    });
+    option.classList.add('active');
+    // Update the active device and adjust aspect ratio
+    setActiveDevice(device);
+    // Update the image list based on the selected project and device
+    const activeProject = document.querySelector('#project-list li.active').dataset.project;
+    updateImages(projects[activeProject][device]);
   });
 });
 
@@ -112,16 +150,16 @@ deviceOptions.forEach((option) => {
 function updateImages(imageList) {
   // Hide all images
   images.forEach((img) => {
-      img.classList.remove('active');
+    img.classList.remove('active');
   });
 
   // Display images for the selected project and device
   imageList.forEach((imageName, index) => {
-      images[index].src = imageName;
+    images[index].src = imageName;
 
-      // Update alt attribute following the specified format
-      const activeProject = document.querySelector('#project-list li.active').dataset.project;
-      images[index].alt = `${activeProject}_${activeDevice}_image${index + 1}`;
+    // Update alt attribute following the specified format
+    const activeProject = document.querySelector('#project-list li.active').dataset.project;
+    images[index].alt = `${activeProject}_${activeDevice}_image${index + 1}`;
   });
 
   // Start with the first image displayed
@@ -129,22 +167,31 @@ function updateImages(imageList) {
   images[currentIndex].classList.add('active');
 }
 
-// Function to update the active device and adjust aspect ratio
+// Function to update the active device
 function setActiveDevice(device) {
   activeDevice = device;
-  // Update aspect ratio for all images
-  images.forEach((img) => {
-      img.style.aspectRatio = aspectRatios[activeDevice];
-  });
 }
 
-// Initialize with the first project and its images for the default device (tablet)
-const initialProject = projectListItems[0].dataset.project;
-updateImages(projects[initialProject][activeDevice]);
-projectListItems[0].classList.add('active');
-updateAspectRatio(activeDevice); // Set the initial aspect ratio to the default device (tablet)
-setActiveDevice(activeDevice); // Set the initial device option as active
+// Function to set the default project, device, and display images
+function setDefaultProjectAndDevice() {
+  // Select the first project in the list and mark it as active
+  const firstProjectItem = projectListItems[0];
+  firstProjectItem.classList.add('active');
 
+  // Set the default device to "Tablet" and mark it as active
+  const defaultDeviceOption = document.querySelector('nav li[data-device="tablet"]');
+  defaultDeviceOption.classList.add('active');
 
-//To avoid stretching of the image, do this
-//on windows load, let Tablet be clicked and the first project be selected by default.
+  // Get the selected project and device
+  const selectedProject = firstProjectItem.dataset.project;
+  const selectedDevice = defaultDeviceOption.dataset.device;
+
+  // Update the image list based on the selected project and device
+  updateImages(projects[selectedProject][selectedDevice]);
+}
+
+// Call the function to set the default project and device after the DOM has loaded
+document.addEventListener('DOMContentLoaded', setDefaultProjectAndDevice);
+
+// Call the function initially to set device options based on the initial screen width
+updateDeviceOptions();
