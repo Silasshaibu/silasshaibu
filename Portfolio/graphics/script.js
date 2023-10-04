ee4b24963ba4cc26024699b6231953c8e286e798
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-10-04 14:53:40 +0100

    commit

--- Portfolio/graphics/script.js
+++ Portfolio/graphics/script.js
@@ -91,6 +91,42 @@ function loadProjectsIntoGrid() {
         const gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
 
+        const gridItemOverlay = document.createElement('div');
+        gridItemOverlay.className = 'grid-item-overlay';
+
+        // Check if the project contains a video
+        let hasVideo = false;
+        for (let j = 0; j < project.images.length; j++) {
+            if (typeof project.images[j].imageUrl === 'string' && project.images[j].imageUrl.endsWith('.mp4')) {
+                hasVideo = true;
+                break; // Exit the loop as soon as a video is found
+            }
+        }
+
+        gridItemOverlay.innerHTML = `
+            <div class="overlay">
+                <div class="overlaySpanFlex">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="svgMediaIcons">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                        </svg>
+                    </span>
+                    <span style='display:${hasVideo ? 'block' : 'none'}'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="svgMediaIcons">
+                        <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                        </svg>
+                    </span>
+                </div>
+                <div style="bottom:1rem; padding:.5rem; position:absolute; color:white;">
+                    <strong>${project.name}</strong><br>
+                    <h3 style="font-weight:300; font-size:.9em;">Silas S.</h3>
+                </div>
+            </div>
+        `;
+
+        // Creating GridItemOverlay and putting contents inside to be rendered dynamically
+        gridItem.appendChild(gridItemOverlay);
+
         project.images.forEach((image) => {
             const imageContainer = document.createElement('div');
             imageContainer.className = 'image-container';
@@ -138,7 +174,7 @@ function HireMe() {
 // Function to open the modal and populate it with project data
 function openModal(project, index) {
     modalImages.innerHTML = ""; // Clear previous images
-    const currentImages = project.images; //Store the current project Image
+    const currentImages = project.images; // Store the current project Image
 
     for (let i = 0; i < project.images.length; i++) {
         const imageContainer = document.createElement('div'); // Create a div container for the image
@@ -196,7 +232,7 @@ function openModal(project, index) {
 
     // Clearing the project information before adding software tags
     projectInformation.innerHTML = '';
-    for (w in project.softwareUsed) {
+    for (let w = 0; w < project.softwareUsed.length; w++) {
         const softwares = document.createElement('span');
         softwares.textContent = project.softwareUsed[w];
         projectInformation.appendChild(softwares);
@@ -259,3 +295,19 @@ function goToPreviousProject() {
 leftArrow.addEventListener('click', goToPreviousProject);
 rightArrow.addEventListener('click', goToNextProject);
 
+// Function to close the modal when clicking outside of it
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+// Function to close the modal when pressing the Escape key
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
+
+// Call the function when the page is loaded
+window.addEventListener('load', loadProjectsIntoGrid);
