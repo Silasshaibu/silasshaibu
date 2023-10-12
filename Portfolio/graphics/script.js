f31e431c83169b49771d7ec9d3dcf16816f4a463
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-10-12 04:41:53 +0100

    commit

--- Portfolio/graphics/script.js
+++ Portfolio/graphics/script.js
@@ -172,9 +172,120 @@ function HireMe() {
 }
 
 // Function to open the modal and populate it with project data
+// function openModal(project, index) {
+//     modalImages.innerHTML = ""; // Clear previous images
+//     const currentImages = project.images; // Store the current project Image
+
+//     for (let i = 0; i < project.images.length; i++) {
+//         const imageContainer = document.createElement('div'); // Create a div container for the image
+//         imageContainer.className = 'image-container';
+
+//         if (project.images[i].imageUrl.endsWith('.jpg')) {
+//             imageContainer.innerHTML = `
+//                 <img src="${project.images[i].imageUrl}" alt="${project.name}" />
+//                 <div class="caption"><p>${project.images[i].caption}</p></div>
+//             `;
+
+//             const actionSection = document.createElement('div');
+//             actionSection.className = 'inner-div';
+//             actionSection.innerHTML = `
+//                 <span class="DownloadButton">
+//                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+//                         <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"></path>
+//                     </svg>
+//                 </span>
+//                 <span class="ViewFullScreen">
+//                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+//                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"></path>
+//                     </svg>
+//                 </span>
+//             `;
+//             imageContainer.appendChild(actionSection);
+//         }
+
+//         if (project.images[i].imageUrl.endsWith('.mp4')) {
+//             imageContainer.innerHTML = `
+//                 <video controls controlslist="nofullscreen nodownload" width="100%">
+//                     <source src="${project.images[i].imageUrl}"  type="video/mp4" />
+//                 </video>
+//                 <div class="caption"><p>${project.images[i].caption}</p></div>
+//             `;
+//         }
+
+//         const advertImage = document.querySelector('.advert');
+//         advertImage.innerHTML = `<img src='${project.advertImage}' alt='advert' width="100%">`;
+
+//         // Add image, and image container to modalImages div
+//         modalImages.appendChild(imageContainer);
+//     }
+
+//     // Set the project name
+//     const projectID = modal.querySelector('.projectNumber');
+//     projectID.textContent = project.id;
+
+//     // Set the project description
+//     const projectDescription = modal.querySelector('.authorInfo.aboutProject');
+//     projectDescription.innerHTML = project.description;
+
+//     // Set Software Used InfoSection
+//     const projectInformation = modal.querySelector('.projectInformationSoftware');
+
+//     // Clearing the project information before adding software tags
+//     projectInformation.innerHTML = '';
+//     for (let w = 0; w < project.softwareUsed.length; w++) {
+//         const softwares = document.createElement('span');
+//         softwares.textContent = project.softwareUsed[w];
+//         projectInformation.appendChild(softwares);
+//     }
+
+//     // Set ProjectAboutTitle
+//     const projectAbout = modal.querySelector('.projectInformation.Description');
+//     projectAbout.innerHTML = `<h4>${project.name}</h4>`;
+
+//     // Display the modal
+//     modal.style.display = 'flex';
+//     currentProjectIndex = index;
+
+//     // Get the Download and View Full Screen buttons
+//     const downloadButton = modal.querySelector('.DownloadButton');
+//     const viewFullScreenButton = modal.querySelector('.ViewFullScreen');
+
+//     // Add click event listener for Download button
+//     downloadButton.addEventListener('click', () => {
+//         const currentImage = modalImages.querySelector('img');
+//         if (currentImage) {
+//             const imageSrc = currentImage.getAttribute('src');
+//             // Create an anchor element to trigger the download
+//             const downloadLink = document.createElement('a');
+//             downloadLink.href = imageSrc;
+//             downloadLink.download = `${project.name}_SilasShaibu_Portfolio.jpg`; // Set the desired download filename
+//             downloadLink.click();
+//         }
+//     });
+
+//     // Add click event listener for View Full Screen button
+//     viewFullScreenButton.addEventListener('click', () => {
+//         const currentImage = modalImages.querySelector('img');
+//         if (currentImage) {
+//             const imageSrc = currentImage.getAttribute('src');
+//             // Open a new window/tab with the image
+//             const newWindow = window.open(imageSrc, '_blank', 'fullscreen=yes');
+//             if (newWindow) {
+//                 newWindow.document.body.style.margin = '0';
+//                 newWindow.document.body.style.overflow = 'hidden';
+//             }
+//         }
+//     });
+
+//     updateProjectInformation();
+// }
+
 function openModal(project, index) {
     modalImages.innerHTML = ""; // Clear previous images
     const currentImages = project.images; // Store the current project Image
+
+    const advertImage = document.querySelector('.advert');
+    advertImage.innerHTML = `<img src='${project.advertImage}' alt='advert' width="100%">`;
 
     for (let i = 0; i < project.images.length; i++) {
         const imageContainer = document.createElement('div'); // Create a div container for the image
@@ -196,26 +307,48 @@ function openModal(project, index) {
                 </span>
                 <span class="ViewFullScreen">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"></path>
-                    </svg>
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"></path>
+                </svg>
                 </span>
             `;
             imageContainer.appendChild(actionSection);
+
+            // Get the Download and View Full Screen buttons for this image
+            const downloadButton = imageContainer.querySelector('.DownloadButton');
+            const viewFullScreenButton = imageContainer.querySelector('.ViewFullScreen');
+
+            // Add event listeners for Download button
+            downloadButton.addEventListener('click', () => {
+                const imageSrc = project.images[i].imageUrl;
+                // Create an anchor element to trigger the download
+                const downloadLink = document.createElement('a');
+                downloadLink.href = imageSrc;
+                downloadLink.download = `${project.name}_${i}_SilasShaibu_Portfolio.jpg`; // Set a unique download filename
+                downloadLink.click();
+            });
+
+            // Add event listeners for View Full Screen button
+            viewFullScreenButton.addEventListener('click', () => {
+                const imageSrc = project.images[i].imageUrl;
+                // Open a new window/tab with the image
+                const newWindow = window.open(imageSrc, '_blank', 'fullscreen=yes');
+                if (newWindow) {
+                    newWindow.document.body.style.margin = '0';
+                    newWindow.document.body.style.overflow = 'hidden';
+                }
+            });
         }
 
         if (project.images[i].imageUrl.endsWith('.mp4')) {
             imageContainer.innerHTML = `
                 <video controls controlslist="nofullscreen nodownload" width="100%">
-                    <source src="${project.images[i].imageUrl}"  type="video/mp4" />
+                    <source src="${project.images[i].imageUrl}" type="video/mp4" />
                 </video>
                 <div class="caption"><p>${project.images[i].caption}</p></div>
             `;
         }
 
-        const advertImage = document.querySelector('.advert');
-        advertImage.innerHTML = `<img src='${project.advertImage}' alt='advert' width="100%">`;
-
-        // Add image, and image container to modalImages div
+        // Add the image container to modalImages div
         modalImages.appendChild(imageContainer);
     }
 
@@ -246,39 +379,9 @@ function openModal(project, index) {
     modal.style.display = 'flex';
     currentProjectIndex = index;
 
-    // Get the Download and View Full Screen buttons
-    const downloadButton = modal.querySelector('.DownloadButton');
-    const viewFullScreenButton = modal.querySelector('.ViewFullScreen');
-
-    // Add click event listener for Download button
-    downloadButton.addEventListener('click', () => {
-        const currentImage = modalImages.querySelector('img');
-        if (currentImage) {
-            const imageSrc = currentImage.getAttribute('src');
-            // Create an anchor element to trigger the download
-            const downloadLink = document.createElement('a');
-            downloadLink.href = imageSrc;
-            downloadLink.download = `${project.name}_SilasShaibu_Portfolio.jpg`; // Set the desired download filename
-            downloadLink.click();
-        }
-    });
-
-    // Add click event listener for View Full Screen button
-    viewFullScreenButton.addEventListener('click', () => {
-        const currentImage = modalImages.querySelector('img');
-        if (currentImage) {
-            const imageSrc = currentImage.getAttribute('src');
-            // Open a new window/tab with the image
-            const newWindow = window.open(imageSrc, '_blank', 'fullscreen=yes');
-            if (newWindow) {
-                newWindow.document.body.style.margin = '0';
-                newWindow.document.body.style.overflow = 'hidden';
-            }
-        }
-    });
-
     updateProjectInformation();
 }
+
 
 // Function to navigate to the next project
 function goToNextProject() {
